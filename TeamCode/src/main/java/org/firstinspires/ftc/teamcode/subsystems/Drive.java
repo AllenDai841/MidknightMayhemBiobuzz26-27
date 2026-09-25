39da4376a54834bc5bec5fc1a7fde4aef5902503
Allen Dai <allendai000@gmail.com>
2026-09-25 16:45:53 -0700

    fix the drives idk what happened

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drive.java
@@ -3,13 +3,12 @@ package org.firstinspires.ftc.teamcode.subsystems;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-public class DriveTest {
+public class Drive {
     private DcMotorEx fl;
     private DcMotorEx fr;
     private DcMotorEx bl;
     private DcMotorEx br;
-
-    public DriveTest(HardwareMap hardwareMap) {
+    public Drive(HardwareMap hardwareMap) {
         fl = hardwareMap.get(DcMotorEx.class, "fl");
         fr = hardwareMap.get(DcMotorEx.class, "fr");
         bl = hardwareMap.get(DcMotorEx.class, "bl");
@@ -22,7 +21,6 @@ public class DriveTest {
         bl.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
         br.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
     }
-
     public void drive(double forward, double strafe, double turn) {
         double flPower = forward + strafe + turn;
         double frPower = forward - strafe - turn;
@@ -44,9 +42,5 @@ public class DriveTest {
         fr.setPower(frPower/max);
         bl.setPower(blPower/max);
         br.setPower(brPower/max);
-    }
-
-    public void stop() {
-        drive(0, 0, 0);
     }
 }
